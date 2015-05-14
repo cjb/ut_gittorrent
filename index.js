32814e1cf7b3dbda3fe1787034db306f4f6b594f
Chris Ball <chris@printf.net>
2015-05-14 13:48:54 -0400

    Decode responses

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* jshint camelcase: false */
 
+var bencode = require('bencode')
 var EventEmitter = require('events').EventEmitter
 var inherits = require('inherits')
 
@@ -17,16 +18,22 @@ module.exports = function () {
   ut_gitswarm.prototype.name = 'ut_gitswarm'
 
   ut_gitswarm.prototype.onExtendedHandshake = function (handshake) {
+    console.error('onExtendedHandshake for gitswarm')
+    console.error(handshake)
     var self = this
-    if (!handshake.m || !handshake.m.ut_pex) {
-      return self.emit('warning', new Error('Peer does not support ut_pex'))
+    if (!handshake.m || !handshake.m.ut_gitswarm) {
+      return self.emit('warning', new Error('Peer does not support ut_gitswarm'))
+    }
+    if (!self.handshook) {
+      self.emit('handshake')
+      self.handshook = true
     }
   }
 
   ut_gitswarm.prototype.ask = function(sha1) {
     var self = this
-    console.log("asking for " + sha1)
-    message = sha1
+    console.error("asking for " + sha1)
+    var message = {sha: sha1}
     self._sendMessage(message)
   }
 
@@ -34,9 +41,22 @@ module.exports = function () {
     var self = this
     var message
 
-    console.log("in onMessage")
-    console.log(buf)
+    console.error("in onMessage")
+    console.error(buf.toString())
+
+    var dict, trailer
+    try {
+      var str = buf.toString()
+      var trailerIndex = str.indexOf('ee') + 2
+      dict = bencode.decode(str.substring(0, trailerIndex))
+      trailer = buf.slice(trailerIndex)
+    } catch (err) {
+      // drop invalid messages
+      return
+    }
   }
+
+  console.error(dict)
 
   ut_gitswarm.prototype._sendMessage = function (message) {
     var self = this
