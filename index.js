99f248686a2fabde3631171bd0bbf2498bc6a6a6
Chris Ball <chris@printf.net>
2015-05-27 16:55:08 -0400

    Rename!  gitswarm->gittorrent

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ var fs = require('fs');
 
 module.exports = function () {
 
-  inherits(ut_gitswarm, EventEmitter)
+  inherits(ut_gittorrent, EventEmitter)
 
-  function ut_gitswarm (wire) {
+  function ut_gittorrent (wire) {
     var self = this
     EventEmitter.call(self)
 
     self._wire = wire
   }
 
-  ut_gitswarm.prototype.name = 'ut_gitswarm'
+  ut_gittorrent.prototype.name = 'ut_gittorrent'
 
-  ut_gitswarm.prototype.onExtendedHandshake = function (handshake) {
+  ut_gittorrent.prototype.onExtendedHandshake = function (handshake) {
     var self = this
-    if (!handshake.m || !handshake.m.ut_gitswarm) {
-      return self.emit('warning', new Error('Peer does not support ut_gitswarm'))
+    if (!handshake.m || !handshake.m.ut_gittorrent) {
+      return self.emit('warning', new Error('Peer does not support ut_gittorrent'))
     }
     if (!self.handshook) {
       self.emit('handshake')
@@ -29,42 +29,42 @@ module.exports = function () {
     }
   }
 
-  ut_gitswarm.prototype.ask = function(sha1) {
+  ut_gittorrent.prototype.ask = function(sha1) {
     var self = this
     var message = {sha: sha1}
     self._sendMessage(message)
   }
 
-  ut_gitswarm.prototype.sendTorrent = function (infoHash) {
+  ut_gittorrent.prototype.sendTorrent = function (infoHash) {
     var self = this
     var message = {infoHash: infoHash}
     self._sendMessage(message)
   }
 
-  ut_gitswarm.prototype.onMessage = function (buf) {
+  ut_gittorrent.prototype.onMessage = function (buf) {
     var self = this
     var message
 
     var dict = bencode.decode(buf.toString())
 
-    if (dict.gitswarm.sha) {
-      var sha = dict.gitswarm.sha.toString()
+    if (dict.gittorrent.sha) {
+      var sha = dict.gittorrent.sha.toString()
       self.emit('generatePack', sha)
       return
     }
 
-    if (dict.gitswarm.infoHash) {
-      self.emit('receivedTorrent', dict.gitswarm.infoHash.toString())
+    if (dict.gittorrent.infoHash) {
+      self.emit('receivedTorrent', dict.gittorrent.infoHash.toString())
       return
     }
   }
 
-  ut_gitswarm.prototype._sendMessage = function (message) {
+  ut_gittorrent.prototype._sendMessage = function (message) {
     var self = this
-    self._wire.extended('ut_gitswarm', {
-      'gitswarm': message
+    self._wire.extended('ut_gittorrent', {
+      'gittorrent': message
     })
   }
 
-  return ut_gitswarm
+  return ut_gittorrent
 }
--- test/basic.js
+++ test/basic.js
@@ -1,16 +1,16 @@
 /* jshint camelcase: false */
 
 var Protocol = require('bittorrent-protocol')
-var ut_gitswarm = require('../')
+var ut_gittorrent = require('../')
 var test = require('tape')
 
-test('wire.use(ut_gitswarm())', function (t) {
+test('wire.use(ut_gittorrent())', function (t) {
   var wire = new Protocol()
   wire.pipe(wire)
 
-  wire.use(ut_gitswarm())
+  wire.use(ut_gittorrent())
 
-  t.ok(wire.ut_gitswarm)
+  t.ok(wire.ut_gittorrent)
   t.end()
 })
 
