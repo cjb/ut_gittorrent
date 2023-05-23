9433efaff40ec184156eead455bad9ad6b8131e5
dzdidi <deniszalessky@gmail.com>
2023-05-23 15:30:07 +0200

    test: check methods exists
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- test/basic.js
+++ test/basic.js
@@ -11,5 +11,11 @@ test('wire.use(ut_gittorrent())', (t) => {
   wire.use(utGittorrent())
 
   t.ok(wire.ut_gittorrent)
+  t.ok(wire.ut_gittorrent.onExtendedHandshake)
+  t.ok(wire.ut_gittorrent.ask)
+  t.ok(wire.ut_gittorrent.sendTorrent)
+  t.ok(wire.ut_gittorrent.onMessage)
+  t.ok(wire.ut_gittorrent._sendMessage)
+
   t.end()
 })
